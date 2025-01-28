5087299684582b24824896b37c5500d9653b41e0
Tony Masek <tm@tony-masek.com>
2025-01-28 19:45:14 +0100

    Made a mod on the chat

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+mod chat;
+use chat::init_chat_loop as init_chat_loop;
 
 fn main() {
     println!("Enter your name:");
@@ -12,69 +14,4 @@ fn main() {
     let name = name.trim().to_string();
 
     init_chat_loop(name);
-}
-
-fn init_chat_loop(name: String) {
-    let mut index = 0;
-    let consent = get_consent();
-    let mut answers = Vec::new();
-    answers.push(name);
-
-    while consent {
-        println!("{}", ask_question(index));
-        let mut answer = String::new();
-
-        io::stdin()
-            .read_line(&mut answer)
-            .expect("Failed to read line");
-
-        answers.push(answer.trim().to_string());
-
-        if index >= 3 {
-            print_answers(answers);
-            break;
-        }
-
-        index += 1;
-    }
-}
-
-fn get_consent() -> bool {
-    println!("Would you like to give us more?");
-
-    let mut consent = String::new();
-
-    io::stdin()
-        .read_line(&mut consent)
-        .expect("Well that did not work...");
-
-    let consent = consent.trim();
-
-    if consent == "Yes" || consent == "Y" {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-fn ask_question(index: usize) -> &'static str {
-    if index < 3 {
-        let questions = vec![
-            "How old are you?",
-            "What exactly is your take on google?",
-            "Did you know, that an ostrich is not my favourite animal?",
-        ];
-        return questions[index];
-    } else {
-        return "Yea... I don't want to know anything else... Just come back later...";
-    }
-}
-
-fn print_answers(answers: Vec<String>) {
-    let mut index = 1;
-    while index != answers.len() {
-        println!("Answer no. {}: {}", index, answers[index]);
-        index += 1;
-    }
-    println!("Not bad... 10/10")
 }
